345b2d223d21b9e2463ad8a6770c02df12156ddc
staciax <stacia.dev@gmail.com>
2023-12-29 03:36:23 +0700

    tag fixes

--- backend/apps/tags/urls.py
+++ backend/apps/tags/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns: list[path] = []
+from . import views
+
+urlpatterns: list[path] = [
+    path('', views.TagList.as_view()),
+    path('<int:id>/', views.TagDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/apps/tags/views.py
+++ backend/apps/tags/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from django.http import Http404
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Tag
+from .serializers import TagSerializer
+
+
+class TagList(APIView):  # type: ignore
+    def get(self, request: Request) -> Response:
+        tag = Tag.objects.all()
+        serializer = TagSerializer(tag, many=True)
+        return Response(serializer.data)
+
+    def post(self, request: Request) -> Response:
+        serializer = TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TagDetail(APIView):  # type: ignore
+    def get_tag(self, id: int) -> Any:
+        try:
+            return Tag.objects.get(id=id)
+        except Tag.DoesNotExist:
+            raise Http404
+
+    def get(self, request: Request, id: int) -> Response:
+        tag = self.get_tag(id)
+        serializer = TagSerializer(tag)
+        return Response(serializer.data)
+
+    def put(self, request: Request, id: int) -> Response:
+        tag = self.get_tag(id)
+        serializer = TagSerializer(tag, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, id: int) -> Response:
+        tag = self.get_tag(id)
+        tag.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
