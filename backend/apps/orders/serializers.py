2550d44330789df527d471ed12131d0d079483e8
staciax <stacia.dev@gmail.com>
2023-12-29 01:22:18 +0700

    update orders app

--- backend/apps/orders/serializers.py
+++ backend/apps/orders/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+
+from .models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):  # type: ignore
+    class Meta:
+        model = Order
+        fields = '__all__'
--- backend/apps/orders/views.py
+++ backend/apps/orders/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from django.http import Http404
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Order
+from .serializers import OrderSerializer
+
+
+class OrderList(APIView):  # type: ignore
+    def get(self, request: Request) -> Response:
+        order = Order.objects.all()
+        serializer = OrderSerializer(order, many=True)
+        return Response(serializer.data)
+
+    def post(self, request: Request) -> Response:
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class OrderDetail(APIView):  # type: ignore
+    def get_order(self, id: int) -> Any:
+        try:
+            return Order.objects.get(id=id)
+        except Order.DoesNotExist:
+            raise Http404
+
+    def get(self, request: Request, id: int) -> Response:
+        order = self.get_order(id)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    def put(self, request: Request, id: int) -> Response:
+        order = self.get_order(id)
+        serializer = OrderSerializer(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, id: int) -> Response:
+        order = self.get_order(id)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
