4ee04b49787ca2b61d3912f42db1b960ca87fbb2
STACIA <69411527+staciax@users.noreply.github.com>
2023-12-28 19:34:34 +0700

    add `carts` table

---
+++ backend/apps/carts/models.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+from django.db import models
+
+from apps.customers.models import Customer
+
+
+class Cart(models.Model):  # type: ignore
+    id = models.AutoField(primary_key=True)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, related_name='cart')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'Cart of {self.customer}'
+
+    @property
+    def customer_id(self) -> Any:
+        return self.customer.id
+
+
+class CartItem(models.Model):  # type: ignore
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.IntegerField()
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def cart_id(self) -> Any:
+        return self.cart.id
+
+    @property
+    def product_id(self) -> Any:
+        return self.product.id
+
+    # def get_total(self) -> float:
+    #     return self.product.price * self.quantity
