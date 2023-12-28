5524ae212518c3fa5403d239b19dd3da1c1110db
STACIA <69411527+staciax@users.noreply.github.com>
2023-12-28 20:35:52 +0700

    demo address apps

--- backend/apps/address/urls.py
+++ backend/apps/address/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 
-urlpatterns: list[path] = []
+from . import views
+
+urlpatterns: list[path] = [
+    path('', views.root, name='root'),
+    path('address/', views.get_address, name='get_address'),
+]
--- backend/apps/address/views.py
+++ backend/apps/address/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
+from .models import Address
+from .serializers import AddressSerializer
 
 # Create your views here.
+
+
+class AddressList(APIView):  # type: ignore
+    def get(self, request: HttpRequest) -> JsonResponse:
+        ...
+
+    def post(self, request: HttpRequest) -> JsonResponse:
+        ...
+
+    def put(self, request: HttpRequest) -> JsonResponse:
+        ...
+
+    def delete(self, request: HttpRequest) -> JsonResponse:
+        ...
+
+
+def root(request: HttpRequest) -> JsonResponse:
+    return JsonResponse({'message': 'Welcome to the address app!'}, status=200)
+
+
+@api_view(['GET'])  # type: ignore
+def get_address(request: HttpRequest) -> JsonResponse:
+    address = Address.objects.all()
+    serializer = AddressSerializer(address, many=True)
+    return JsonResponse(serializer.data, safe=False)
