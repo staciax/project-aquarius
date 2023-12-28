e8b60c8422117691f45cd1be862c991eca03f370
staciax <stacia.dev@gmail.com>
2023-12-29 03:34:32 +0700

    address fixes

--- backend/apps/address/models.py
+++ backend/apps/address/models.py
@@ -14,7 +14,7 @@ class Address(models.Model):  # type: ignore
     phone_number = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    customer = models.ForeignKey(
+    customer = models.OneToOneField(
         'customers.Customer',
         on_delete=models.CASCADE,
         related_name='address',
