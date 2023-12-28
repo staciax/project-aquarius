c97e77215ce8a7afd2ae1dd443b29fa3cc36a713
STACIA <69411527+staciax@users.noreply.github.com>
2023-12-28 19:11:08 +0700

    alter genres table

--- backend/apps/genres/models.py
+++ backend/apps/genres/models.py
@@ -1,7 +1,13 @@
-from django.db import models
+from __future__ import annotations
 
-# Create your models here.
+from django.db import models
 
 
 class Genre(models.Model):  # type: ignore
+    # if TYPE_CHECKING:
+    #     products: models.ManyToManyField[Product]
+
     id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
