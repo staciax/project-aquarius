64368709791ce3a66ca8d332815bbdd36522ba2c
STACIA <69411527+staciax@users.noreply.github.com>
2023-12-28 19:34:11 +0700

    alter table of orders

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -2,14 +2,14 @@ from typing import Any
 
 from django.db import models
 
+from apps.customers.models import Customer
 from apps.payments.models import Payment
 from apps.products.models import Product
 
 
 class Order(models.Model):  # type: ignore
     id = models.AutoField(primary_key=True)
-    customer_id = models.IntegerField()
-    status = models.CharField(max_length=128)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None, null=True, related_name='orders')
     payment = models.OneToOneField(
         Payment,
         on_delete=models.CASCADE,
@@ -17,6 +17,7 @@ class Order(models.Model):  # type: ignore
         default=None,
         related_name='order',
     )
+    status = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     preparing_at = models.DateTimeField(null=True, default=None)
@@ -24,13 +25,25 @@ class Order(models.Model):  # type: ignore
     delivered_at = models.DateTimeField(null=True, default=None)
     cancelled_at = models.DateTimeField(null=True, default=None)
 
+    @property
+    def customer_id(self) -> Any:
+        return self.customer.id
+
+    @property
+    def payment_id(self) -> Any:
+        return self.payment.id
+
 
 class OrderItem(models.Model):  # type: ignore
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def order_id(self) -> Any:
+        return self.order.id
 
     @property
     def product_id(self) -> Any:
