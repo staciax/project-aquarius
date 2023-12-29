728bd08c8f36f11df0db29660566a0ff57aefe5b
staciax <stacia.dev@gmail.com>
2023-12-30 00:49:10 +0700

    set media root

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -139,6 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
