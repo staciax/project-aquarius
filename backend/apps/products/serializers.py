43e3ae314e6a400b982a436fa5332deffcdad72c
staciax <stacia.dev@gmail.com>
2023-12-29 16:33:29 +0700

    update product model serializer
    # renamed attrs
    `tag_ids` to `tag_names`
    
    # set max length
    - tag_names to `5`
    - genre_ids to `5`
    
    # improve update method

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -58,11 +58,13 @@ class ProductSerializer(serializers.ModelSerializer):  # type: ignore
         child=serializers.IntegerField(),
         write_only=True,
         required=False,
+        max_length=5,
     )
-    tag_ids = serializers.ListField(
+    tag_names = serializers.ListField(
         child=serializers.CharField(),
         write_only=True,
         required=False,
+        max_length=5,
     )
 
     class Meta:
@@ -80,23 +82,68 @@ class ProductSerializer(serializers.ModelSerializer):  # type: ignore
             'genres',
             'tags',
             'genre_ids',
-            'tag_ids',
+            'tag_names',
         )
 
     def create(self, validated_data: dict[str, Any]) -> Any:
-        genre_ids = validated_data.pop('genre_ids', [])
-        tag_ids = validated_data.pop('tag_ids', [])
-        product = Product.objects.create(**validated_data)
+        product = Product.objects.create(
+            name=validated_data['name'],
+            description=validated_data['description'],
+            price=validated_data['price'],
+            isbn=validated_data['isbn'],
+        )
 
+        # set default inventory
         product.inventory = ProductInventory.objects.create(product=product)
 
+        # add genre to product
+        genre_ids = validated_data.get('genre_ids', [])
         for genre_id in genre_ids:
             product.genres.add(genre_id)
 
-        for tag_id in tag_ids:
-            tag, _ = Tag.objects.get_or_create(name=tag_id)
+        # add tag to product
+        tag_names = validated_data.get('tag_names', [])
+        for tname in tag_names:
+            tag, _ = Tag.objects.get_or_create(name=tname)
             product.tags.add(tag)
+
         return product
 
-    def update(self, instance: Any, validated_data: dict[str, Any]) -> Any:
-        return super().update(instance, validated_data)
+    def update(self, instance: Product, validated_data: dict[str, Any]) -> Any:
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.price = validated_data.get('price', instance.price)
+        instance.isbn = validated_data.get('isbn', instance.isbn)
+
+        # genres
+
+        genre_ids = validated_data.get('genre_ids', [])
+
+        # remove genres that are not in the genre_ids
+        for genre in instance.genres.all():
+            if genre.id not in genre_ids:
+                instance.genres.remove(genre)
+            else:
+                genre_ids.remove(genre.id)
+
+        # add genres that are in the genre_ids
+        for genre_id in genre_ids:
+            instance.genres.add(genre_id)
+
+        # tags
+
+        tag_names = validated_data.get('tag_names', [])
+
+        for tag in instance.tags.all():
+            if tag.name not in tag_names:
+                instance.tags.remove(tag)
+            else:
+                tag_names.remove(tag.name)
+
+        for tname in tag_names:
+            tag, _ = Tag.objects.get_or_create(name=tname)
+            instance.tags.add(tag)
+
+        instance.save()
+
+        return instance
