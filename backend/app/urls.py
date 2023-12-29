c322c1e8b7a2bc34e7dc40c0344e2fa4f4f17295
staciax <stacia.dev@gmail.com>
2023-12-29 21:41:08 +0700

    add static and media url

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -14,20 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
-    path('address/', include('apps.address.urls')),
-    path('carts/', include('apps.carts.urls')),
-    path('customers/', include('apps.customers.urls')),
-    path('genres/', include('apps.genres.urls')),
-    path('orders/', include('apps.orders.urls')),
-    path('payments/', include('apps.payments.urls')),
-    path('product_genres/', include('apps.product_genres.urls')),
-    path('product_tags/', include('apps.product_tags.urls')),
-    path('products/', include('apps.products.urls')),
-    path('tags/', include('apps.tags.urls')),
-]
+urlpatterns = (
+    [
+        path('admin/', admin.site.urls),
+        # path('api-auth/', include('rest_framework.urls')),
+        path('address/', include('apps.address.urls')),
+        path('carts/', include('apps.carts.urls')),
+        path('customers/', include('apps.customers.urls')),
+        path('genres/', include('apps.genres.urls')),
+        path('orders/', include('apps.orders.urls')),
+        path('payments/', include('apps.payments.urls')),
+        path('product_genres/', include('apps.product_genres.urls')),
+        path('product_tags/', include('apps.product_tags.urls')),
+        path('products/', include('apps.products.urls')),
+        path('tags/', include('apps.tags.urls')),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
