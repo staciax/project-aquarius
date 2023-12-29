16f5a536b1e17fb34e9127168072da1c3d9d7bfd
staciax <stacia.dev@gmail.com>
2023-12-29 15:28:23 +0700

    tag name to unique

--- backend/apps/tags/models.py
+++ backend/apps/tags/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Tag(models.Model):  # type: ignore
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
