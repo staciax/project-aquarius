874915ccbabb45307a9d7b881af0d159e7f53ee9
staciax <stacia.dev@gmail.com>
2023-12-30 21:01:33 +0700

    improve payments api view

--- backend/apps/payments/views.py
+++ backend/apps/payments/views.py
@@ -1,10 +1,4 @@
-from typing import Any
-
-from django.http import Http404
-from rest_framework import generics, status
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Payment
 from .serializers import PaymentSerializer
@@ -15,27 +9,8 @@ class PaymentList(generics.ListCreateAPIView):  # type: ignore
     serializer_class = PaymentSerializer
 
 
-class PaymentDetail(APIView):  # type: ignore
-    def get_payment(self, id: int) -> Any:
-        try:
-            return Payment.objects.get(id=id)
-        except Payment.DoesNotExist:
-            raise Http404
-
-    def get(self, request: Request, id: int) -> Response:
-        payment = self.get_payment(id)
-        serializer = PaymentSerializer(payment)
-        return Response(serializer.data)
-
-    def put(self, request: Request, id: int) -> Response:
-        payment = self.get_payment(id)
-        serializer = PaymentSerializer(payment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request: Request, id: int) -> Response:
-        payment = self.get_payment(id)
-        payment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class PaymentDetail(generics.RetrieveUpdateDestroyAPIView):  # type: ignore
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'id'
