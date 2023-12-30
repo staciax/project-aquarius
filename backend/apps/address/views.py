0b3cffa9ffbd6956742a8d713766385f0f545271
staciax <stacia.dev@gmail.com>
2023-12-30 14:32:44 +0700

    improvements api views

--- backend/apps/address/views.py
+++ backend/apps/address/views.py
@@ -1,10 +1,4 @@
-from typing import Any
-
-from django.http import Http404
-from rest_framework import generics, status
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Address
 from .serializers import AddressSerializer
@@ -15,27 +9,7 @@ class AddressList(generics.ListCreateAPIView):  # type: ignore
     serializer_class = AddressSerializer
 
 
-class AddressDetail(APIView):  # type: ignore
-    def get_address(self, id: int) -> Any:
-        try:
-            return Address.objects.get(id=id)
-        except Address.DoesNotExist:
-            raise Http404
-
-    def get(self, request: Request, id: int) -> Response:
-        address = self.get_address(id)
-        serializer = AddressSerializer(address)
-        return Response(serializer.data)
-
-    def put(self, request: Request, id: int) -> Response:
-        address = self.get_address(id)
-        serializer = AddressSerializer(address, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request: Request, id: int) -> Response:
-        address = self.get_address(id)
-        address.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class AddressDetail(generics.RetrieveUpdateDestroyAPIView):  # type: ignore
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    lookup_field = 'id'
--- backend/apps/tags/views.py
+++ backend/apps/tags/views.py
@@ -1,10 +1,7 @@
 from typing import Any
 
 from django.http import Http404
-from rest_framework import generics, status
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Tag
 from .serializers import TagSerializer
@@ -15,27 +12,7 @@ class TagList(generics.ListCreateAPIView):  # type: ignore
     serializer_class = TagSerializer
 
 
-class TagDetail(APIView):  # type: ignore
-    def get_tag(self, id: int) -> Any:
-        try:
-            return Tag.objects.get(id=id)
-        except Tag.DoesNotExist:
-            raise Http404
-
-    def get(self, request: Request, id: int) -> Response:
-        tag = self.get_tag(id)
-        serializer = TagSerializer(tag)
-        return Response(serializer.data)
-
-    def put(self, request: Request, id: int) -> Response:
-        tag = self.get_tag(id)
-        serializer = TagSerializer(tag, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request: Request, id: int) -> Response:
-        tag = self.get_tag(id)
-        tag.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):  # type: ignore
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    lookup_field = 'id'
