8213e3995cfee45c6dd79241bf3bd3e28b29d275
staciax <stacia.dev@gmail.com>
2023-12-31 17:26:36 +0700

    remove unused

--- backend/apps/genres/models.py
+++ backend/apps/genres/models.py
@@ -1,12 +1,7 @@
-from __future__ import annotations
-
 from django.db import models
 
 
 class Genre(models.Model):  # type: ignore
-    # if TYPE_CHECKING:
-    #     products: models.ManyToManyField[Product]
-
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
