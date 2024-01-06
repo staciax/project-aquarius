3add867c9f7fcb311b68d1d8fd34a8e493f334b2
staciax <stacia.dev@gmail.com>
2024-01-06 21:14:44 +0700

    static and media enable only debug

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -24,11 +24,12 @@ handler403 = 'app.exceptions.forbidden'
 handler404 = 'app.exceptions.not_found'
 handler500 = 'app.exceptions.server_error'
 
-urlpatterns = (
-    [
-        path('admin/', admin.site.urls),
-        path('', include('api.urls')),
-    ]
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
