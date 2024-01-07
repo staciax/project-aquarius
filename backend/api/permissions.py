232de8430b0adb44bfa68bc3f194a3782bd73c46
staciax <stacia.dev@gmail.com>
2024-01-07 21:46:25 +0700

    add custom permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,17 +1,32 @@
 from rest_framework import permissions
 from rest_framework.request import Request
+from rest_framework.views import APIView
+
+from api.user.models import User
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):  # type: ignore
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
+class IsSuperUser(permissions.BasePermission):
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        return bool(request.user and request.user.is_superuser)
 
-    def has_object_permission(self, request: Request, view: str, obj: str) -> bool:
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
 
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.owner == request.user  # type: ignore
+class BaseAPIPermission(permissions.DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        return super().has_permission(request, view)
+
+
+class IsCustomer(BaseAPIPermission):
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        if not isinstance(request.user, User):
+            return False
+        return super().has_permission(request, view) and request.user.is_customer
