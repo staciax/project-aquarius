a076a52e4e707c4d789c39d845db9a91ea5b5c7a
staciax <stacia.dev@gmail.com>
2024-01-07 21:10:23 +0700

    add jwt auth url

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 handler400 = 'app.exceptions.bad_request'
 handler403 = 'app.exceptions.forbidden'
@@ -26,9 +27,11 @@ handler500 = 'app.exceptions.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('', include('api.urls'), name='api'),
+    path('auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
