e599612e2f5c9122cbea9ee7a57a24dd2bf806cc
staciax <stacia.dev@gmail.com>
2024-01-10 00:06:39 +0700

    api permission fixes

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from rest_framework import permissions
 from rest_framework.request import Request
 from rest_framework.views import APIView
@@ -6,13 +8,21 @@ from api.user.models import User
 from app.permissions import BaseAPIPermission
 
 
-class IsSuperUser(permissions.BasePermission):  # type: ignore
+class IsSuperUser(permissions.IsAuthenticated):  # type: ignore
     def has_permission(self, request: Request, view: APIView) -> bool:
         return bool(request.user and request.user.is_superuser)
 
 
+class IsSuperUserOrReadOnly(IsSuperUser):  # type: ignore
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        return bool(request.method in permissions.SAFE_METHODS or super().has_permission(request, view))
+
+
 class IsCustomer(BaseAPIPermission):
     def has_permission(self, request: Request, view: APIView) -> bool:
-        if not isinstance(request.user, User):
+        assert isinstance(request.user, User)
+        if request.user.is_superuser:
+            return True
+        if request.user.is_staff:
             return False
         return super().has_permission(request, view) and request.user.is_customer
